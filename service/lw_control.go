3212ce9c06d5cfaacd86e3510ad3393acd939237
Ewout Prangsma <ewout@prangsma.net>
2021-10-17 13:29:11 +0200

    More logs

--- service/lw_control.go
+++ service/lw_control.go
@@ -175,12 +175,17 @@ func (s *service) GetOutputRequests(req *api.OutputRequestsOptions, server api.L
 		select {
 		case msg := <-ch:
 			if moduleID == "" || hasMatchingModuleID(msg.GetAddress(), moduleID) {
-				s.Log.Debug().
+				log.Debug().
 					Str("address", string(msg.GetAddress())).
 					Str("to", moduleID).
 					Int32("value", msg.GetRequest().GetValue()).
 					Msg("Send output request")
 				if err := server.Send(&msg); err != nil {
+					log.Warn().
+						Str("address", string(msg.GetAddress())).
+						Str("to", moduleID).
+						Int32("value", msg.GetRequest().GetValue()).
+						Msg("Send output request failed")
 					return err
 				}
 				if !req.GetManualConfirm() {
